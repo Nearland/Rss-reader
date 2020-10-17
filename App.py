65c265bc109da4415ff00e703cd3cfd1b9237984
k1_243_02 <dfsd>
2020-10-17 10:48:55 +0300

    new life

---
+++ App.py
@@ -0,0 +1,91 @@
+import os
+import tkinter
+
+import feedparser
+import sys  # sys нужен для передачи argv в QApplication
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.uic.properties import QtCore
+from array import *
+
+import designe  # Это наш конвертированный файл дизайна
+
+
+
+
+
+class ExampleApp(QtWidgets.QMainWindow, designe.Ui_MainWindow):
+    def __init__(self):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        self.pushButton.clicked.connect(self.myFunction)
+        self.urltext.setPlaceholderText("Введите url-rss адрес...")
+
+        # Пока пустая функция которая выполняется
+        # при нажатии на кнопку
+    def myFunction(self):
+        def parseRSS(rss_url):
+            return feedparser.parse(rss_url)
+
+        def getHeadlines(rss_url):
+            headlines = []
+
+            feed = parseRSS(rss_url)
+            for newsitem in feed['items']:
+                if self.checkBox_2.isChecked():
+                    headlines.append(newsitem['title'])
+                if self.checkBox_3.isChecked():
+                    headlines.append(newsitem['link'])
+                if self.checkBox_4.isChecked():
+                    headlines.append(newsitem['id'])
+                if self.checkBox_5.isChecked():
+                    headlines.append(newsitem['summary'])
+                if self.checkBox_6.isChecked():
+                    headlines.append(newsitem['published'])
+
+            return headlines
+
+        allheadlines = []
+
+        newsurls = {
+
+            'googlenews': 'https://news.google.com/rss?hl=ru&gl=RU&ceid=RU:ru',
+
+        }
+
+        for key, url in newsurls.items():
+            allheadlines.extend(getHeadlines(url))
+            listOfHeads = []
+            s = ""
+        if self.urltext.toPlainText() == 'https://news.google.com/rss?hl=ru&gl=RU&ceid=RU:ru':
+            for hl in allheadlines:
+                s += hl + "\n"
+                # listOfHeads.append(hl)
+                self.outinfo.setPlainText(s)
+                print("-", hl)
+                # dannie = self.urltext.toPlainText()
+                # self.outinfo.setPlainText(hl)
+                #while True:
+                    #print("-", hl)
+                    #self.outinfo.setText(hl)
+                    #break
+        else:
+            from tkinter import messagebox
+            root = tkinter.Tk()
+            root.withdraw()
+            messagebox.showinfo("Ошибка", "Неправильный url-rss адрес")
+            print("lol")
+        print(listOfHeads)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+    window = ExampleApp()  # Создаём объект класса ExampleApp
+    window.show()  # Показываем окно
+    app.exec_()  # и запускаем приложение
+
+
+if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
+    main()  # то запускаем функцию main()
---
+++ Rss-reader.py
@@ -0,0 +1,35 @@
+import feedparser
+
+
+def parseRSS(rss_url):
+    return feedparser.parse(rss_url)
+
+
+def getHeadlines(rss_url):
+    headlines = []
+
+    feed = parseRSS(rss_url)
+    for newsitem in feed['items']:
+        headlines.append(newsitem['title'])
+        #headlines.append(newsitem['link'])
+        #headlines.append(newsitem['id'])
+        #headlines.append(newsitem['summary'])
+        #headlines.append(newsitem['published'])
+
+    return headlines
+
+
+allheadlines = []
+
+newsurls = {
+
+    'googlenews': 'https://news.google.com/rss?hl=ru&gl=RU&ceid=RU:ru',
+
+
+}
+
+for key, url in newsurls.items():
+    allheadlines.extend(getHeadlines(url))
+
+for hl in allheadlines:
+    print(hl)
